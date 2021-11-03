325ce4d0600cd93cabcfece9f142b7cff070a174
Jeremiah Smith <jeresmith@live.com>
2021-11-03 14:04:02 -0700

    Game mode added

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,69 @@
 import java.util.*;
+
 public class Main {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
 
-         StringChecking c = new StringChecking();
-         ShuntingYard s = new ShuntingYard();
-         ReversePolish rev = new ReversePolish();
+        gameRules();
+        game();
 
-         String ss = "3*-(-(-2))";
-
-         if (c.checkForAll(ss))
-         {
-             Queue<String> l = s.runShuntingYard(ss);
-             Queue<String> k = s.convertString(ss);
-             double res = rev.runAlgorithm(l);
-            System.out.println(res);
-
-         }
     }
+
+    public static void gameRules()
+    {
+        System.out.println("GAME RULES");
+        System.out.println("-----------");
+        System.out.println("Enter e to exit");
+        System.out.println("-----------");
+        System.out.println("No spaces");
+        System.out.println("-----------");
+        System.out.println("Unary and trig functions require parenthesis");
+        System.out.println("-----------");
+        System.out.println();
+    }
+    
+    public static void game()
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        StringChecking c = new StringChecking();
+        ShuntingYard s = new ShuntingYard();
+        ReversePolish rev = new ReversePolish();
+        boolean exit = false;
+
+        while (!exit)
+        {
+
+            String input = "";
+            System.out.println("Enter a calculation");
+            System.out.print(">> ");
+            input = keyboard.nextLine();
+            System.out.println();
+            if (input.equals("e"))
+            {
+                exit = true;
+                continue;
+            }
+
+            boolean valid = c.checkForAll(input);
+
+            if (!valid)
+            {
+                System.out.println("Error");
+            } else {
+                Queue<String> l = s.runShuntingYard(input);
+                double res = rev.runAlgorithm(l);
+                System.out.println("Answer: " + res);
+
+            }
+            System.out.println();
+
+        }
+
+        System.out.println("Program ended");
+
+    }
+
 }
 
 
